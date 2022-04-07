f82b0a013fad82c7393378aea63bd50242249abc
Conrad <conradh5656@gmail.com>
2022-04-07 14:20:17 +0100

    Implemented bootstrap

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
     echo <<<_END
         <footer>
             <h3>Thanks for visiting!</h3>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
             <script src="scripts/custom.js"></script>
         </footer>   
     _END;
--- header.php
+++ header.php
@@ -7,8 +7,10 @@
         <!DOCTYPE html>
         <html lang="en">
         <head>
-            <title>Movie Database</title>
-            <link rel='stylesheet' id='stylesheet' href='css/stylesheet.css'"
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Movie Database</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel='stylesheet' id='stylesheet' href='css/stylesheet.css'"
         </head>
     
         <body>
