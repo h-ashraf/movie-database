176aad9db31cd1841741edb8bc6e4efb61184081
h-ashraf <81376919+h-ashraf@users.noreply.github.com>
2022-04-15 15:42:09 +0100

    Update credentials.php

--- credentials.php
+++ credentials.php
@@ -3,4 +3,13 @@ $dbhost  = 'localhost';
 $dbuser  = 'root';
 $dbpass  = '';
 $dbname  = 'movie_database';
+
+// Create connection
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
