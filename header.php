66e83823934110de5dd1353cddacec8749b577b3
h-ashraf <81376919+h-ashraf@users.noreply.github.com>
2022-04-15 15:48:37 +0100

    Update header.php

--- header.php
+++ header.php
@@ -9,14 +9,21 @@
         <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Movie Database</title>
+        //CSS
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel='stylesheet' id='stylesheet' href='css/stylesheet.css'"
         </head>
-    
-        <body>
-
-            <header>
-                <h1>Movie Database</h1>
-            </header>
+        
+        <header>
+            //Navbar
+            <nav class="active">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#about">About</a></li>
+            </nav>
+            
+            // add search bar in here
+            
+            
+        </header>
     _END;
 ?>
