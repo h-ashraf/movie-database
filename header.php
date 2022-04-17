65773c9e8244196cc730347c4a819b83826749e6
h-ashraf <81376919+h-ashraf@users.noreply.github.com>
2022-04-17 11:56:36 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,29 +1,27 @@
-<?php
-    require_once "credentials.php";
-    session_start();
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+        <?php
+        require_once "credentials.php";
+        session_start();
+        ?>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Movie Database</title>
+		//CSS
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+		<link rel="stylesheet" id="stylesheet" href="css/stylesheet.css" />
+	</head>
 
-    echo <<<_END
+	<header>
+		//Navbar
+		<input type="checkbox" id="menuButton" />
+		<label for="menuButton" class="menu-button-label">
+			<div class="white-bar"></div>
+			<div class="white-bar"></div>
+			<div class="white-bar"></div>
+			<div class="white-bar"></div>
+		</label>
 
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Movie Database</title>
-        //CSS
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel='stylesheet' id='stylesheet' href='css/stylesheet.css'"
-        </head>
-        
-        <header>
-            //Navbar
-            <nav class="active">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="#about">About</a></li>
-            </nav>
-            
-            // add search bar in here
-            
-            
-        </header>
-    _END;
-?>
+		// add search bar in here
+	</header>
+</html>
