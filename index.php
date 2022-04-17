69d59e561756ab22aa989e8b08757834f3ee0f11
h-ashraf <81376919+h-ashraf@users.noreply.github.com>
2022-04-17 11:38:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <!DOCTYPE html>
 <html lang='en'>
     //add stylesheet link
-    
+    <link rel="stylesheet" href="css/style.css">
     <?php
-    inlcude(header.php)
+    inlcude "header.php";
     ?>
+    
+    <body>
+    </body>
+    
+    <footer>
+        <?php
+        inlcude "footer.php";
+        ?>
+    </footer>
 </html>
